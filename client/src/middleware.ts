3695b2e664bc7702565f762f4d1cd5f285693c7d
eddie wang <wang8119@gmail.com>
2025-01-09 18:32:22 +0800

    feat:中間件動態路由

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,31 +1,40 @@
-import { NextResponse } from 'next/server';
-import { auth } from './auth';
-import { authRoutes, publicRoutes } from './routes';
+import { NextResponse } from 'next/server'
+import { auth } from './auth'
+import { authRoutes, publicRoutes } from './routes'
 
-export default auth((req) =>{
-    const {nextUrl} = req;
-    const isLoggedIn = !!req.auth;
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
 
-    const isPublic = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-    if (isPublic) {
-        return NextResponse.next();
+  const isPublic = publicRoutes.some((route) => {
+    // 如果路由包含正則表達式字符，直接使用它作為模式
+    if (route.includes('\\')) {
+      const regex = new RegExp(`^${route}$`)
+      return regex.test(nextUrl.pathname)
     }
+    return route === nextUrl.pathname
+  })
 
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return NextResponse.redirect(new URL('/members', nextUrl))
-        }
-        return NextResponse.next();
-    }
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
-    if (!isPublic && !isLoggedIn) {
-        return NextResponse.redirect(new URL('/login', nextUrl))
+  if (isPublic) {
+    return NextResponse.next()
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/members', nextUrl))
     }
+    return NextResponse.next()
+  }
+
+  if (!isPublic && !isLoggedIn) {
+    return NextResponse.redirect(new URL('/login', nextUrl))
+  }
+
+  return NextResponse.next()
 })
 
-
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)'],
 }
--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,11 +1,9 @@
-export const publicRoutes = [
-    '/'
-];
+export const publicRoutes = ['/', '/projects', '/projects/\\d+']
 
 export const authRoutes = [
-    '/login',
-    '/register',
-    '/verify-email',
-    '/forgot-password',
-    '/reset-password'
-];
+  '/login',
+  '/register',
+  '/verify-email',
+  '/forgot-password',
+  '/reset-password',
+]
