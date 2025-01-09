38ab83f99bdad22e5d2ec78181f9f6dfb293d475
eddie wang <wang8119@gmail.com>
2025-01-09 16:40:53 +0800

    feat: 整理 register-form

--- client/src/components/auth/register-form.tsx
+++ client/src/components/auth/register-form.tsx
@@ -6,8 +6,16 @@ import { Button, Input } from '@nextui-org/react'
 import { RegisterSchema, registerSchema } from '@/lib/schemas/registerSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { registerUser } from '@/actions/authActions'
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function RegisterForm() {
+  const router = useRouter()
   const {
     register,
     handleSubmit,
@@ -22,6 +30,7 @@ export default function RegisterForm() {
 
     if (result.status === 'success') {
       console.log('user register successful ')
+      router.push('/projects')
     } else {
       if (Array.isArray(result.error)) {
         result.error.forEach((e) => {
@@ -34,44 +43,60 @@ export default function RegisterForm() {
     }
   }
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
-      <Input
-        defaultValue=""
-        label="Name"
-        variant="bordered"
-        {...register('name')}
-        isInvalid={!!errors.name}
-        errorMessage={errors.name?.message as string}
-      />
-      <Input
-        defaultValue=""
-        label="Email"
-        variant="bordered"
-        {...register('email')}
-        isInvalid={!!errors.email}
-        errorMessage={errors.email?.message as string}
-      />
-      <Input
-        defaultValue=""
-        label="Password"
-        variant="bordered"
-        type="password"
-        {...register('password')}
-        isInvalid={!!errors.password}
-        errorMessage={errors.password?.message as string}
-      />
-      {errors.root?.serverError && (
-        <p className="text-danger text-sm">{errors.root.serverError.message}</p>
-      )}
-      <Button
-        isLoading={isSubmitting}
-        isDisabled={!isValid}
-        fullWidth
-        color="secondary"
-        type="submit"
-      >
-        Register
-      </Button>
-    </form>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle className="text-center text-2xl">
+          註冊室內工程手帳
+        </DialogTitle>
+        <div className="text-center">
+          已經已經有帳號？
+          <Link href="/" className="text-yellow-500 hover:underline">
+            登入
+          </Link>
+        </div>
+      </DialogHeader>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
+        <Input
+          defaultValue=""
+          label="Name"
+          variant="bordered"
+          {...register('name')}
+          isInvalid={!!errors.name}
+          errorMessage={errors.name?.message as string}
+        />
+        <Input
+          defaultValue=""
+          label="Email"
+          variant="bordered"
+          {...register('email')}
+          isInvalid={!!errors.email}
+          errorMessage={errors.email?.message as string}
+        />
+        <Input
+          defaultValue=""
+          label="Password"
+          variant="bordered"
+          type="password"
+          {...register('password')}
+          isInvalid={!!errors.password}
+          errorMessage={errors.password?.message as string}
+        />
+        {errors.root?.serverError && (
+          <p className="text-danger text-sm">
+            {errors.root.serverError.message}
+          </p>
+        )}
+        <Button
+          isLoading={isSubmitting}
+          isDisabled={!isValid}
+          fullWidth
+          color="secondary"
+          type="submit"
+        >
+          Register
+        </Button>
+      </form>
+    </DialogContent>
   )
 }
