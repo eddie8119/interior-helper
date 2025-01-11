45ebc4a2b8b781d75054982199438967aa5257fd
eddie wang <wang8119@gmail.com>
2025-01-12 00:41:40 +0800

    auth callbacks

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -9,6 +9,16 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks:{
+    async session({token, session}){     
+      if(token.sub && session.user) {
+        session.user.id = token.sub
+      }   
+       // NextAuth 默認的 session 不包含用戶 ID
+       // 我們需要用戶 ID 來進行數據庫操作和權限驗證
+      return session
+    }
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
