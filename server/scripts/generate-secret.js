b1a56ac31af10cdbe09b139098407e3d20db02de
eddie wang <wang8119@gmail.com>
2025-01-26 23:26:13 +0800

    移除 server

--- server/scripts/generate-secret.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-// 生成 64 字節的隨機密鑰
-const secret = crypto.randomBytes(64).toString('hex');
-
-console.log('Generated JWT_SECRET:');
-console.log(secret);
-console.log('\nCopy this value to your .env file:');
-console.log(`JWT_SECRET=${secret}`);
