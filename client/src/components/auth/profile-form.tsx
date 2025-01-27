3100beb473effe369b47577e3d3e3fd0590a58d3
eddie wang <wang8119@gmail.com>
2025-01-27 21:52:52 +0800

    add ProfileForm

--- client/src/components/auth/profile-form.tsx
+++ client/src/components/auth/profile-form.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
+import { TextField } from '@mui/material'
+import { useFormContext } from 'react-hook-form'
 
 export default function ProfileForm() {
-  return <div>profile-form</div>
+  const {
+    register,
+    getValues,
+    formState: { errors },
+  } = useFormContext()
+
+  return (
+    <>
+      <TextField
+        defaultValue={getValues('company')}
+        label="Company"
+        variant="outlined"
+        {...register('company')}
+        error={!!errors.company}
+        helperText={errors.company?.message as string}
+      />
+      <TextField
+        defaultValue={getValues('city')}
+        label="City"
+        variant="outlined"
+        {...register('city')}
+        error={!!errors.city}
+        helperText={errors.city?.message as string}
+      />
+      <TextField
+        defaultValue={getValues('description')}
+        label="Description"
+        variant="outlined"
+        type="description"
+        {...register('description')}
+        error={!!errors.description}
+        helperText={errors.description?.message as string}
+      />
+    </>
+  )
 }
